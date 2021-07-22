bf4bfaef50335f9036df77004389f521cb692bd0
masterchief164 <63920595+masterchief164@users.noreply.github.com>
2021-07-23 02:38:17 +0530

    added absolute form values

--- src/components/selectorg.js
+++ src/components/selectorg.js
@@ -1,17 +1,27 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import "./style.css";
 import {Context} from "../Store";
 
 const SelectOrg = () => {
     const [Orgs] = useContext(Context);
+    const [selectedOrg,setSelectedOrg] = useState("Select an Organisation")
     console.log(Orgs);
+    const handleSubmit = (event)=>{
+        event.preventDefault();
+
+    }
+    const handleChange=(event)=>{
+        const val = event.target.value;
+        setSelectedOrg(val)
+        console.log(val);
+    }
     return (
         <article className="dropContainer">
-            <select className="dropdown">
-                <option>Select an Organisation</option>
-                {Orgs.map((org, index) => <option key={index}>{org}</option>)}
+            <select className="dropdown" value={selectedOrg} onChange={handleChange}>
+                <option value={0}>Select an Organisation</option>
+                {Orgs.map((org, index) => <option value={index+1} key={index}>{org}</option>)}
             </select>
-            <button type="submit" className="dropBtn">Submit</button>
+            <button type="submit" onSubmit={handleSubmit} className="dropBtn">Submit</button>
         </article>
     )
 }
