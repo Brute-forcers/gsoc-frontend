4f58f2c6a880bf99a2d10247b141e355e366a10b
masterchief164 <63920595+masterchief164@users.noreply.github.com>
2021-07-23 11:44:32 +0530

    preview button fixed

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./style.css";
 import Issue from "./issue";
 import PreviewInfo from "./previewInfo";
@@ -6,14 +6,15 @@ import PreviewInfo from "./previewInfo";
 const Repos = ({repo}) => {
     const Issues = repo.issues;
     const [preview, setPreview] = React.useState(false);
+    const [issueWorking,setIssueWorking] = useState(repo.issues[0])
     return (
         <>
-            {preview ? <PreviewInfo setPreview={setPreview}/> : (
+            {preview ? <PreviewInfo setPreview={setPreview} detail = {issueWorking.title}/> : (
                 <div className="repoContainer">
                     <h3>{repo.repoName}</h3>
                     <div className="issuesContainer">
                         {Issues.map((issue, index) => {
-                            return <Issue key={index} issue={issue} setPreview={setPreview}/>
+                            return <Issue key={index} issue={issue} setPreview={setPreview} setWorking = {setIssueWorking}/>
                         })}
                     </div>
                 </div>)}
--- src/components/issue.js
+++ src/components/issue.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import "./style.css";
 
-const Issue = ({issue,setPreview}) => {
+const Issue = ({issue,setPreview,setWorking}) => {
     const clickHandler = () =>{
+        setWorking(issue)
         setPreview(true);
     }
     return (
         <article className="issueContainer">
          <img src="./static/git-icon.svg" alt="Organization icon" className="svg"/>
          <h3>#{issue.number}</h3>
-         <p className="desktop-only">This is an issue related to this. Please fix the issue.</p>
+         <p className="desktop-only">{issue.title}</p>
          <span>Good first issue</span>
          <button onClick={clickHandler}>Info</button>
          <div className="mobile-only">
