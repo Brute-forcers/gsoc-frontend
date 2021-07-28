0aa720f52885fa1a82988e8bb066096033b3b3da
masterchief164 <63920595+masterchief164@users.noreply.github.com>
2021-07-28 14:10:32 +0530

    parsing markdown in details styling left

--- src/components/previewInfo.js
+++ src/components/previewInfo.js
@@ -1,16 +1,16 @@
 import React from 'react'
+import Markdown from "markdown-to-jsx";
 
-const PreviewInfo = ({setPreview,detail}) => {
+const PreviewInfo = ({setPreview, detail}) => {
 
-    const clickHandler = () =>{
-      setPreview(false);
+    const clickHandler = () => {
+        setPreview(false);
     }
-
     return (
         <div className="previewInfo">
-            <article className="previewCon">
-                <div>{detail}</div>
-            </article>
+            <div className="previewCon">
+                <Markdown className='mdPar' options={{wrapper:'div'}} children={detail.body}/>
+            </div>
             <button onClick={clickHandler}>Return</button>
         </div>
     )
